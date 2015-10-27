c2bfd332ec2e8fbeef4064c8f944a366253ad022
Andrei Lapshin <alapshin@fastmail.com>
2015-10-27 16:53:51 +0300

    Fixed problem with neverending observable in RxPresenter
    RxPresenter uses combineLatest to combine view subject and data
    observable. CombineLatest waits for all its children to complete before
    emiting onComplete. Fix this using materialize on source observable and
    dematerialize on combined observable.

--- arctor-lib/src/main/java/com/alapshin/arctor/presenter/rx/delivery/DeliverLatest.java
+++ arctor-lib/src/main/java/com/alapshin/arctor/presenter/rx/delivery/DeliverLatest.java
@@ -1,5 +1,6 @@
 package com.alapshin.arctor.presenter.rx.delivery;
 
+import rx.Notification;
 import rx.Observable;
 import rx.functions.Func1;
 import rx.functions.Func2;
@@ -17,18 +18,19 @@ public class DeliverLatest<T> implements Observable.Transformer<T, T> {
         return Observable
                 .combineLatest(
                         semaphore,
-                        observable,
-                        new Func2<Boolean, T, T>() {
+                        observable.materialize(),
+                        new Func2<Boolean, Notification<T>, Notification<T>>() {
                             @Override
-                            public T call(Boolean flag, T value) {
-                                return flag.booleanValue() ? value : null;
+                            public Notification<T> call(Boolean flag, Notification<T> notification) {
+                                return flag.booleanValue() ? notification : null;
                             }
                         })
-                .filter(new Func1<T, Boolean>() {
+                .filter(new Func1<Notification<T>, Boolean>() {
                     @Override
-                    public Boolean call(T value) {
-                        return value != null;
+                    public Boolean call(Notification<T> notification) {
+                        return notification != null;
                     }
-                });
+                })
+                .dematerialize();
     }
 }
