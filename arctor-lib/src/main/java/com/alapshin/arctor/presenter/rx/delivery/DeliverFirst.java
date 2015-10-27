37da7b8c7ce92df56323a07af0430a5e9fec01aa
Andrei Lapshin <alapshin@fastmail.com>
2015-10-27 16:53:51 +0300

    Fixed problem with neverending observable in RxPresenter
    RxPresenter uses combineLatest to combine view subject and data
    observable. CombineLatest waits for all its children to complete before
    emiting onComplete. Fix this using materialize on source observable and
    dematerialize on combined observable.

--- arctor-lib/src/main/java/com/alapshin/arctor/presenter/rx/delivery/DeliverFirst.java
+++ arctor-lib/src/main/java/com/alapshin/arctor/presenter/rx/delivery/DeliverFirst.java
@@ -17,14 +17,14 @@ public class DeliverFirst<T> implements Observable.Transformer<T, T> {
     public Observable<T> call(Observable<T> observable) {
         return Observable
                 .combineLatest(
-                semaphore,
-                observable,
-                    new Func2<Boolean, T, T>() {
-                        @Override
-                        public T call(Boolean flag, T value) {
-                            return flag.booleanValue() ? value : null;
-                        }
-                    })
+                        semaphore,
+                        observable,
+                        new Func2<Boolean, T, T>() {
+                            @Override
+                            public T call(Boolean flag, T value) {
+                                return flag.booleanValue() ? value : null;
+                            }
+                        })
                 .filter(new Func1<T, Boolean>() {
                     @Override
                     public Boolean call(T value) {
