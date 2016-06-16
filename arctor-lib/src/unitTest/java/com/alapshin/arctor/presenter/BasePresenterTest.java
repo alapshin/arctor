003db9027169e8153f02b67ddf4253ecb7f59141
Andrei Lapshin <alapshin@fastmail.com>
2016-06-16 19:35:27 +0300

    Add tests for BasePresenter

---
+++ arctor-lib/src/unitTest/java/com/alapshin/arctor/presenter/BasePresenterTest.java
@@ -0,0 +1,34 @@
+package com.alapshin.arctor.presenter;
+
+
+import com.alapshin.arctor.view.MvpView;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class BasePresenterTest {
+    @Test
+    public void shouldAttachView() {
+        MvpView view = mock(MvpView.class);
+        BasePresenter<MvpView> presenter = new BasePresenter<>();
+        presenter.attachView(view);
+        assertThat(presenter.getView()).isNotNull();
+        assertThat(presenter.isViewAttached()).isTrue();
+    }
+
+    @Test
+    public void shouldDetachView() {
+        MvpView view = mock(MvpView.class);
+        BasePresenter<MvpView> presenter = new BasePresenter<>();
+
+        presenter.attachView(view);
+        assertThat(presenter.getView()).isNotNull();
+        assertThat(presenter.isViewAttached()).isTrue();
+
+        presenter.detachView();
+        assertThat(presenter.getView()).isNull();
+        assertThat(presenter.isViewAttached()).isFalse();
+    }
+}
