39934069a9263b67f691104ccbf0a3c06fd3a9a6
Andrei Lapshin <alapshin@fastmail.com>
2016-06-16 19:34:20 +0300

    Make BasePresenter class non abstract and remove android dependencies

--- arctor-lib/src/main/java/com/alapshin/arctor/presenter/BasePresenter.java
+++ arctor-lib/src/main/java/com/alapshin/arctor/presenter/BasePresenter.java
@@ -1,9 +1,7 @@
 package com.alapshin.arctor.presenter;
 
 import android.support.annotation.CallSuper;
-import android.util.Log;
 
-import com.alapshin.arctor.BuildConfig;
 import com.alapshin.arctor.view.MvpView;
 
 import java.lang.ref.WeakReference;
@@ -17,7 +15,7 @@ import javax.annotation.Nullable;
  * @author alapshin
  * @since 2015-04-18
  */
-public abstract class BasePresenter<V extends MvpView> implements Presenter<V> {
+public class BasePresenter<V extends MvpView> implements Presenter<V> {
     private final String TAG = this.getClass().getSimpleName();
     /**
      * Reference to view. Using weak reference to avoid memory leaks. Before calling any view
@@ -28,74 +26,47 @@ public abstract class BasePresenter<V extends MvpView> implements Presenter<V> {
     @Override
     @CallSuper
     public void onCreate(@Nullable PresenterBundle savedInstanceState) {
-        if (BuildConfig.DEBUG) {
-            Log.d(TAG, "onCreate");
-        }
     }
 
     @Override
     @CallSuper
     public void onStart() {
-        if (BuildConfig.DEBUG) {
-            Log.d(TAG, "onStart");
-        }
     }
 
     @Override
     @CallSuper
     public void onResume() {
-        if (BuildConfig.DEBUG) {
-            Log.d(TAG, "onResume");
-        }
     }
 
     @Override
     @CallSuper
     public void onPause() {
-        if (BuildConfig.DEBUG) {
-            Log.d(TAG, "onPause");
-        }
     }
 
     @Override
     @CallSuper
     public void onSaveInstanceState(@Nonnull PresenterBundle outState) {
-        if (BuildConfig.DEBUG) {
-            Log.d(TAG, "onSaveInstanceState");
-        }
     }
 
     @Override
     @CallSuper
     public void onStop() {
-        if (BuildConfig.DEBUG) {
-            Log.d(TAG, "onStop");
-        }
     }
 
     @Override
     @CallSuper
     public void onDestroy() {
-        if (BuildConfig.DEBUG) {
-            Log.d(TAG, "onDestroy");
-        }
     }
 
     @Override
     @CallSuper
     public void attachView(V view) {
-        if (BuildConfig.DEBUG) {
-            Log.d(TAG, "attachView");
-        }
         this.viewRef = new WeakReference<V>(view);
     }
 
     @Override
     @CallSuper
     public void detachView() {
-        if (BuildConfig.DEBUG) {
-            Log.d(TAG, "detachView");
-        }
         viewRef.clear();
         viewRef = null;
     }
