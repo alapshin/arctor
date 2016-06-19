e6e00a9df7b3e0eae76a2b928236aa83c8dc7e2b
Andrei Lapshin <alapshin@fastmail.com>
2016-06-19 14:30:26 +0300

    Add mechanism for saving view state during orientation changes

--- sample/src/main/java/com/alapshin/arctor/sample/foo/presenter/FooPresenterImpl.java
+++ sample/src/main/java/com/alapshin/arctor/sample/foo/presenter/FooPresenterImpl.java
@@ -1,8 +1,11 @@
 package com.alapshin.arctor.sample.foo.presenter;
 
+import android.util.Log;
+
 import com.alapshin.arctor.presenter.PresenterBundle;
 import com.alapshin.arctor.presenter.rx.RxPresenter;
 import com.alapshin.arctor.sample.foo.view.FooView;
+import com.alapshin.arctor.viewstate.ViewStateCommand;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,21 +17,37 @@ import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
 public class FooPresenterImpl extends RxPresenter<FooView> implements FooPresenter {
+    public static class ProgressCommand implements ViewStateCommand<FooView> {
+        @Override
+        public int type() {
+            return 0;
+        }
+
+        @Override
+        public void execute(FooView fooView) {
+            fooView.showProgress();
+        }
+    }
+
     @Override
     public void onCreate(@Nullable PresenterBundle bundle) {
         super.onCreate(bundle);
         if (bundle == null) {
-            Subscription dataSubscription = Observable.interval(10, 1, TimeUnit.SECONDS)
-                    .compose(deliverLatestCache())
+            Subscription dataSubscription = Observable.interval(5, 1, TimeUnit.SECONDS)
+                    .compose(deliverLatest())
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe(data -> getView().setData(data));
             addSubscription(dataSubscription);
 
+            ProgressCommand command = new ProgressCommand();
+
             Subscription progressSubscription = Observable.just(null)
-                    .compose(deliverLatestCache())
+                    .compose(deliverLatest())
                     .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(res -> getView().showProgress());
+                    .subscribe(res -> {
+                        executeCommand(command, CommandStoreStrategy.ADD);
+                    });
             addSubscription(progressSubscription);
         }
     }
