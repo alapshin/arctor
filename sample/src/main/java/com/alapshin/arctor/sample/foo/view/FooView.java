100eb7fec6e53d8f6fceb96c1932066e67d354cb
Andrei Lapshin <alapshin@fastmail.com>
2016-10-03 18:08:37 +0300

    Add simple annotation processor for ViewStateCommand generation

--- sample/src/main/java/com/alapshin/arctor/sample/foo/view/FooView.java
+++ sample/src/main/java/com/alapshin/arctor/sample/foo/view/FooView.java
@@ -1,8 +1,10 @@
 package com.alapshin.arctor.sample.foo.view;
 
+import com.alapshin.arctor.annotation.GenerateCommand;
 import com.alapshin.arctor.view.MvpView;
 
 public interface FooView extends MvpView {
     void showProgress();
+    @GenerateCommand(0)
     void setData(long data);
 }
