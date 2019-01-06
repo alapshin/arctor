52b8aaa7d249b2a00d0de0a57d748fcac0c622e0
Andrei Lapshin <alapshin@fastmail.com>
2019-01-06 16:52:46 +0300

    Update dependencies
    And fix various problems caused by it.

--- arctor-base/src/test/java/com/alapshin/arctor/presenter/PresenterBundleUtilTest.java
+++ arctor-base/src/test/java/com/alapshin/arctor/presenter/PresenterBundleUtilTest.java
@@ -1,18 +1,13 @@
 package com.alapshin.arctor.presenter;
 
-
 import android.os.Bundle;
-
-import com.alapshin.arctor.BuildConfig;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
 import static org.assertj.core.api.Assertions.*;
 
 @RunWith(RobolectricTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = 25)
 public class PresenterBundleUtilTest {
     @Test
     public void checkBundleInteraction() {
