2487478a64761badfd4a60d7025a7216a550c329
Andrei Lapshin <alapshin@fastmail.com>
2019-01-13 16:23:58 +0300

    Migrate to RxJava2 in sample app

--- app/src/main/java/com/alapshin/arctor/sample/foo/presenter/FooPresenterImpl.java
+++ app/src/main/java/com/alapshin/arctor/sample/foo/presenter/FooPresenterImpl.java
@@ -3,17 +3,17 @@ package com.alapshin.arctor.sample.foo.presenter;
 import android.support.annotation.Nullable;
 
 import com.alapshin.arctor.presenter.PresenterBundle;
-import com.alapshin.arctor.presenter.rx.RxPresenter;
+import com.alapshin.arctor.presenter.rxjava2.RxPresenter;
 import com.alapshin.arctor.sample.foo.view.FooView;
 
 import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 
-import rx.Observable;
-import rx.Subscription;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class FooPresenterImpl extends RxPresenter<FooView> implements FooPresenter {
     @Inject
@@ -24,18 +24,18 @@ public class FooPresenterImpl extends RxPresenter<FooView> implements FooPresent
     public void onCreate(@Nullable PresenterBundle bundle) {
         super.onCreate(bundle);
         if (bundle == null) {
-            Subscription dataSubscription = Observable.interval(5, 1, TimeUnit.SECONDS)
-                    .compose(deliverLatest())
+            Disposable dataSubscription = Observable.interval(5, 1, TimeUnit.SECONDS)
+                    .compose(waitViewLatest())
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe(data -> getView().setData(data));
-            addSubscription(dataSubscription);
+            addDisposable(dataSubscription);
 
-            Subscription progressSubscription = Observable.just(null)
-                    .compose(deliverLatest())
+            Disposable progressSubscription = Observable.just(true)
+                    .compose(waitViewLatest())
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe(res -> getView().showProgress());
-            addSubscription(progressSubscription);
+            addDisposable(progressSubscription);
         }
     }
 }
